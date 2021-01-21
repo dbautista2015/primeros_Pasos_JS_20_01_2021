45bbb2c0a0fb1f2cfa7c839cd87835014c2bb5c0
Diana Patricia Bautista Otalora <diana.bautista.otalora@gmail.com>
2021-01-20 19:57:14 -0500

    tipo datos enero 20

---
+++ main.js
@@ -0,0 +1,40 @@
+//  Tipos de datos 
+
+// string   cadena 
+
+"Diana Patricia"
+'Bautista Otálora'
+
+// number numero
+
+1000
+2,5
+-25
+
+//boolean afirmaciones
+true
+false
+
+
+//array listas
+
+['angel', 'fabian', 'andre']
+[1, 2 , 3, 4]
+[true, false, true, true]
+
+
+// object objeto
+
+{
+    nombre: "Miguel",
+    apellido: "Castro",
+    edad: 23,
+    profesor: true
+}
+
+{
+    nombre: "Diana",
+    apellido: "Bautista",
+    edad: 37,
+    profesor: null
+}
